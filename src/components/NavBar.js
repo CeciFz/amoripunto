034799b0e954d4268c1e753f37d610bd23f9e5a7
Ceci <ceci.fz87@gmail.com>
2022-11-06 12:43:16 -0300

    Fix navBar hidden

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,7 +22,7 @@ function NavBar(){
             <div onClick={handleNav} className="block md:hidden">
                 { !nav ? <AiOutlineClose size={20}/> :  <AiOutlineMenu size={20}/> }
             </div>
-            <div className={ !nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
+            <div className={ !nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 md:hidden" : "fixed left-[-100%]"}>
                 <h1 className="text-3xl font-bold text-[#00df9a] m-8">Amoripunto.</h1>
                 <ul className="uppercase p-4">
                     <li className="p-4 border-b border-gray-600">Inicio</li>
